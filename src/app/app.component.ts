93f6d84e3ab8c382062374dfbc597c9044d45dc7
Natalya Basovska <natalya@Natalyas-MacBook-Air.local>
2017-10-06 12:01:09 +0300

    Add to GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  rows = [
+    {name: 'PC1',
+    content: 12345678910,
+    url:'http://wallpup.com/wp-content/uploads/2013/03/Beautiful-Scenery-Wallpaper.jpg'},
+    {name: 'PC2',
+    content: 12345678910,
+    url:'http://wallpup.com/wp-content/uploads/2013/02/Nature-Wallpaper-Widescreen.jpg'},
+    {name: 'PC3',
+    content: 12345678910,
+    url:'http://wallpup.com/wp-content/uploads/2013/04/Sunrise-Wallpaper.jpg'},
+    {name: 'PC4',
+    content: 12345678910,
+    url:'http://wallpup.com/wp-content/uploads/2013/02/Waterfall-Pond-Nature-Wallpaper.jpg'},
+    {name: 'PC5',
+    content: 12345678910,
+    url:'http://wallpup.com/wp-content/uploads/2013/03/Field-Sunset.jpg'},
+    {name: 'PC6',
+    content: 12345678910,
+    url:'http://wallpup.com/wp-content/uploads/2014/09/magical-forest-hd-wallpaper.jpeg'}
+  ];
+
+  todos = [
+    {name: 'qwertyuiop'},
+    {name: 'qwertyuiop'},
+    {name: 'qwertyuiop'},
+    {name: 'qwertyuiop'},
+    {name: 'qwertyuiop'},
+    {name: 'qwertyuiop'}
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RowComponent } from './row/row.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RowComponent,
+    HeaderComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
